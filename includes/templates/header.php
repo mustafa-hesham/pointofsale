e243ccfccb72919a4003f9e4325a59845045aa5a
Mustafa Hesham <50930361+mustafa-hesham@users.noreply.github.com>
2022-05-28 20:22:52 +0200

    Add files via upload

---
+++ includes/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="<?= $css ?>bootstrap.min.css">
+    <link rel="stylesheet" href="<?= $css ?>style.css">
+    <link rel="stylesheet" href="<?= $css ?>bootstrap4.3.1.min.css">
+    <link rel="stylesheet" href="<?= $css ?>jquery-ui.css">
+    </head>
+<body>
