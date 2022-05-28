a9e791d1817a8fd1624816c0c7861c8b7edcbd15
Mustafa Hesham <50930361+mustafa-hesham@users.noreply.github.com>
2022-05-28 20:22:23 +0200

    Create footer.php

---
+++ includes/templates/footer.php
@@ -0,0 +1,6 @@
+<script src="<?= $js ?>jquery-3.4.1.min.js"></script>
+<script src="<?= $js ?>jquery-ui.min.js"></script>
+<script src="<?= $js ?>bootstrap.bundle.min.js"></script>
+<script src="<?= $js ?>myJS.js"></script>
+</body>
+</html>
