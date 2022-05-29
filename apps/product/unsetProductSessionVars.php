6d2fc00028d5ae969c6be7c33b04bab90b8b17b5
Mustafa Hesham <50930361+mustafa-hesham@users.noreply.github.com>
2022-05-29 15:35:18 +0200

    Update unsetProductSessionVars.php

--- apps/product/unsetProductSessionVars.php
+++ apps/product/unsetProductSessionVars.php
@@ -7,22 +7,4 @@ foreach($_SESSION as $key=>$value){
     if ($key != 'username')
     unset($_SESSION[$key]);
 }
-
-// $_SESSION['Searchterm']              = "";
-// $_SESSION['productName']             = "";
-// $_SESSION['productBarcode']          = "";
-// $_SESSION['productPrice']            = "";
-// $_SESSION['productImage']            = "";
-// $_SESSION['imageName']               = "";
-// $_SESSION['DoesNotExist']            = false;
-// $_SESSION['imageErrors']             = array();
-// $GLOBALS['target_file']              = '';
-// $_SESSION['duplicateProductName']    = false;
-// $_SESSION['duplicatebarcode']        = false;
-// $_SESSION['successfullUpdate']       = false;
-// $_SESSION['successfullAddition']     = false;
-// $_SESSION['duplicateName']           ='';
-// $_SESSION['duplicateProductBarcode'] = '';  
-
-
 ?>
